1b6c37bae8d3dfc8c007db1caeea55940b7480f7
sathvikakotra <sathvikakotra@gmail.com>
2025-04-08 23:57:59 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-50 w-3/4 bg-amber-500 m-auto rounded-2xl mt-10 flex items-center  justify-center font-bold text-2xl text-orange-700'>
+      <nav className='space-y-4 flex flex-col'>
+        <a href='#'>Pinky</a>
+        <a href='#'>Pooky</a>
+        <a href='#'>Pulky</a>
+        <a href='#'>Pukky</a>
+      </nav>
+    </div>
+  )
+}
+
+export default App
